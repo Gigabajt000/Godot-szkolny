960e3306a05c49ab00c35e2c2e2fd13ddfdd3839
Gigabajt000 <nikitadyrcz8@gmail.com>
2025-02-24 17:04:30 +0100

    drawing

--- painter.gd
+++ painter.gd
@@ -1,6 +1,7 @@
 extends TextureRect
 
 @export var image_size := Vector2i(128, 128)
+@export_range(1, 10) var brush_size := 4
 var image: Image
 
 func _ready() -> void:
@@ -10,4 +11,9 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_pressed("draw"):
+		var mouse_position : Vector2i = get_local_mouse_position()
+		for x in brush_size:
+			for y in brush_size:
+				image.set_pixelv(mouse_position + Vector2i(x - brush_size/2, y - brush_size/2), Color.BLACK)
+		texture = ImageTexture.create_from_image(image)
